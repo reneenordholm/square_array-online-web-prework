73565f717375ee946a5df38474e39dd17f24d718
Renee Nordholm <reneenordholm@gmail.com>
2019-03-25 11:19:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,11 @@
 def square_array(array)
-  numbers = [1,2,3]
-  array.each do|num|
-  puts (numbers * 2)
+  array.each_with_object([]) { |i,arr| arr << i ** 2 }
 end
 end
-#puts "Taking out #{apple}"
-#apples_taken_out += 1
+
+#def square_array(array)
+#  numbers = [1,2,3]
+#  array.each do|num|
+#  puts (numbers * 2)
+#end
+#end
