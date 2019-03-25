e1fdcdf2566469d4549a88b82e26373ebf3c9111
Renee Nordholm <reneenordholm@gmail.com>
2019-03-25 11:20:46 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  array.each_with_object([]) { |i,arr| arr << i ** 2 }
 end
+
+#def square_array(array)
+#  numbers = [1,2,3]
+#  array.each do|num|
+#  puts (numbers * 2)
+#end
+#end
